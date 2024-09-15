98c7ac2da9a545a7d31b2cdcb07ba6e0c64b89ed
lunar-huang <43958511+lunar-huang@users.noreply.github.com>
2024-09-15 15:17:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -62,6 +62,6 @@ def borrow():
 
 
 
-
-if __name__=='__main__':
-    app.run()
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # 使用环境变量 PORT 或者默认为 5000
+    app.run(host='0.0.0.0', port=port)
